f1ba9c098a14c7db9bfffaf99f38d7af698a679a
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-03-12 18:36:40 -0300

    configurando o projeto e iniciando a tela de boas vindas

---
+++ help_student/lib/screens/welcome_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:help_student/widgets/customized_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        // decoration: const BoxDecoration(
+        //   image:DecorationImage(
+        //   image: AssetImage("assets/background.png")
+        //   ),
+        // ),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 130,
+            width: 180,
+            child: Image(
+                image: AssetImage("assets/logo.png"),
+                fit: BoxFit.cover
+            ),
+          ),
+         const SizedBox(
+            height: 40,
+          ),
+        CustomizedButton(
+          buttonText: "Login",
+          buttonColor: Colors.black,
+          textColor: Colors.white,
+          onPressed: (){},
+        ),
+          CustomizedButton(
+            buttonText: "Register",
+            buttonColor: Colors.white,
+            textColor: Colors.black,
+            onPressed: (){},
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Continue as a Guest",
+              style: TextStyle(
+                color: Color(0xff35C2C1),
+                fontSize: 25
+              ),
+            ),
+          )
+        ],
+      ),
+      ),
+    );
+  }
+}
