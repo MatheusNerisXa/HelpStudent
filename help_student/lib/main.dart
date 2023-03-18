7dbe7f63e379a4d18203f515c2cd3bca96589b58
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-03-18 14:15:59 -0300

    configurando o firebase

--- help_student/lib/main.dart
+++ help_student/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:help_student/firebase_options.dart';
 import 'package:help_student/screens/welcome_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
